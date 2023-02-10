460722d4dcbc0b88c683999ddece78eb4fb94598
andrechrstian <andrechristianacl@gmail.com>
2023-02-10 21:28:04 +0800

    revisi paymentinspage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import CarDetailPage from './Pages/CarDetailPage';
 import PaymentPage from './Pages/PaymentPage';
 import SigninPage from './Pages/SigninPage';
 import SignupPage from './Pages/SignupPage';
+import PaymentInsPage from './Pages/PaymentInsPage';
+
 
 
 const App = () => {
@@ -16,6 +18,7 @@ const App = () => {
         <Route path='/searchcar' element={<SearchCarsPage />} />
         <Route path='/cardetail/:id' element={<CarDetailPage />} />
         <Route path='/payment' element={<PaymentPage />} />
+        <Route path='/paymentIns' element={<PaymentInsPage/>} />
         <Route path='/SigninPage' element={<SigninPage />} />
         <Route path='/SignupPage' element={<SignupPage />} />
       </Routes>
